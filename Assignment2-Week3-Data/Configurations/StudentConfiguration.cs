d47c2788041f9f3417accd3ba2ff85ebfcc20800
Hoang Van NAm <namhv@gemvietnam.com>
2020-07-07 17:42:27 +0700

    Add project files.

---
+++ Assignment2-Week3-Data/Configurations/StudentConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Assignment2_Week3_Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2_Week3_Data.Configurations
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("Students");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired(true);
+            builder.Property(x => x.Dob).IsRequired(true);
+            builder.Property(x => x.Phone);
+            builder.Property(x => x.Address).IsRequired();
+            builder.Property(x => x.AddressFull).IsRequired();
+        }
+    }
+}
---
+++ Assignment2-Week3-Data/EF/StudentDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Assignment2_Week3_Data.Configurations;
+using Assignment2_Week3_Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2_Week3_Data.EF
+{
+    class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
+            modelBuilder.ApplyConfiguration(new ProvinceConfiguration());
+            modelBuilder.ApplyConfiguration(new DistrictConfiguration());
+            modelBuilder.ApplyConfiguration(new CommuneConfiguration());
+            //base.OnModelCreating(modelBuilder);
+        }
+        
+        public DbSet<Student> Students { set; get; }
+        public DbSet<Province> Provinces { set; get; }
+        public DbSet<District> Districts { set; get; }
+        public DbSet<Commune> Communes { set; get; }
+    }
+}
